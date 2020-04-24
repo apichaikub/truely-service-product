704b38e7e3b536c94690f9cc05e515df75f6d3de
_CHAI__ <apichai.freedom@gmail.com>
2020-04-24 22:11:48 +0700

    test build on `prd` with out connect to `db`.

--- src/server.js
+++ src/server.js
@@ -1,25 +1,33 @@
 import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import typeDefs from './graphql/schema'
-import resolvers from './graphql/resolvers'
-import { sequelize, models } from './config/database'
+// import { ApolloServer } from 'apollo-server-express'
+// import typeDefs from './graphql/schema'
+// import resolvers from './graphql/resolvers'
+// import { sequelize, models } from './config/database'
 
 const app = express()
 const PORT = process.env.PORT
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: { models },
-})
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: { models },
+// })
 
-server.applyMiddleware({ app, path: '/graphql' })
+// server.applyMiddleware({ app, path: '/graphql' })
 
 app.get('/', (req, res) => {
   res.send('Service Product.')
 })
 
-sequelize.authenticate().then(function(err) {
-  app.listen(PORT, () => {
-    console.log(`Running on port: ${PORT}`)
-  })
+app.get('/env', (req, res) => {
+  res.send(process.env)
 })
+
+app.listen(PORT, () => {
+  console.log(`Running on port: ${PORT}`)
+})
+
+// sequelize.authenticate().then(function(err) {
+//   app.listen(PORT, () => {
+//     console.log(`Running on port: ${PORT}`)
+//   })
+// })
