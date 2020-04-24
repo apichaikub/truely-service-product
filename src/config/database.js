0b2e1a0a2db56bcba90d41fa7b016c2df2a03f1a
_CHAI__ <apichai.freedom@gmail.com>
2020-04-24 22:33:04 +0700

    test database on prd.

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,13 @@ import Sequelize from 'Sequelize'
 
 let sequelize = null
 
+// this work on product heroku provide use env `DATABASE_URL`.
+console.log('process.env.DATABASE_URL', process.env.DATABASE_URL)
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
   })
-} else {
+} else { // this work on local development config on docker-compose.
   sequelize = new Sequelize({
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
