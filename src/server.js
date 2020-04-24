775fa1bd25b05e5963fe6ec91ba3f2b6eb112149
_CHAI__ <apichai.freedom@gmail.com>
2020-04-24 22:21:25 +0700

    revert change.

--- src/server.js
+++ src/server.js
@@ -1,33 +1,25 @@
 import express from 'express'
-// import { ApolloServer } from 'apollo-server-express'
-// import typeDefs from './graphql/schema'
-// import resolvers from './graphql/resolvers'
-// import { sequelize, models } from './config/database'
+import { ApolloServer } from 'apollo-server-express'
+import typeDefs from './graphql/schema'
+import resolvers from './graphql/resolvers'
+import { sequelize, models } from './config/database'
 
 const app = express()
 const PORT = process.env.PORT
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: { models },
-// })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: { models },
+})
 
-// server.applyMiddleware({ app, path: '/graphql' })
+server.applyMiddleware({ app, path: '/graphql' })
 
 app.get('/', (req, res) => {
   res.send('Service Product.')
 })
 
-app.get('/env', (req, res) => {
-  res.send(process.env)
+sequelize.authenticate().then(function(err) {
+  app.listen(PORT, () => {
+    console.log(`Running on port: ${PORT}`)
+  })
 })
-
-app.listen(PORT, () => {
-  console.log(`Running on port: ${PORT}`)
-})
-
-// sequelize.authenticate().then(function(err) {
-//   app.listen(PORT, () => {
-//     console.log(`Running on port: ${PORT}`)
-//   })
-// })
