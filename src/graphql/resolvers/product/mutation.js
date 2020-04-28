e69a6efd7a10a81285e4c2c504e4593ea60e3105
_CHAI__ <apichai.freedom@gmail.com>
2020-04-28 21:28:47 +0700

    refactor update product api.

--- src/graphql/resolvers/product/mutation.js
+++ src/graphql/resolvers/product/mutation.js
@@ -1,7 +1,5 @@
 import { combineResolvers } from 'graphql-resolvers'
 import { isLoggedIn, isAdmin } from '../../authorization'
-import { getAllKeysExcept } from '../../../utils/lib'
-import { RESPONSE_STATUS } from '../../../utils/enum'
 
 export default {
   Mutation: {
@@ -9,10 +7,8 @@ export default {
         isLoggedIn,
         isAdmin,
         async (root, args, { models: { Product } }) => {
-          const products = Product.bulkCreate(args.data)
-
           return {
-            products,
+            products: Product.bulkCreate(args.data),
           }
         },
     ),
@@ -21,26 +17,9 @@ export default {
         isLoggedIn,
         isAdmin,
         async (root, args, { models: { Product } }) => {
-          const results = []
-
-          for (const input of args.data) {
-            const values = getAllKeysExcept(input, ['productId', 'skus'])
-            const options = {
-              where: {
-                productId: input.productId,
-              },
-            }
-
-            const product = await Product.update(values, options)
-
-            const result = product[0]?
-              RESPONSE_STATUS.SUCCESS :
-              RESPONSE_STATUS.FAIL
-
-            results.push(result)
-          }
-
-          return results
+          return Product.updateMany(args.data, {
+            primaryId: 'productId',
+          })
         },
     ),
   },
