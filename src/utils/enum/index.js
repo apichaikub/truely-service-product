c9ecfcf0dafb2220d968aa02d3129057885822e0
_CHAI__ <apichai.freedom@gmail.com>
2020-04-28 11:02:03 +0700

    create enum file.

---
+++ src/utils/enum/index.js
@@ -0,0 +1,15 @@
+const RESPONSE_STATUS = Object.freeze({
+  'FAIL': 'FAIL',
+  'SUCCESS': 'SUCCESS',
+})
+
+const USER_ROLE = Object.freeze({
+  'GUEST': 'GUEST',
+  'ADMIN': 'ADMIN',
+})
+
+
+export {
+  RESPONSE_STATUS,
+  USER_ROLE,
+}
