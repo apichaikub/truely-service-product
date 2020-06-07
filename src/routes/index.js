337551b9d799cf5cff0e4d9dfb38741cb66ab981
_CHAI__ <apichai.freedom@gmail.com>
2020-06-07 09:48:22 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,14 @@ router.get('/', (req, res) => {
   res.send('Service Product.')
 })
 
+router.get('/product', (req, res) => {
+  res.send('Test route /product')
+})
+
+router.get('/product/1', (req, res) => {
+  res.send('Test route /product/1')
+})
+
 router.get('*', (req, res) => {
   res.send('Service Product, Not match any routes.')
 })
