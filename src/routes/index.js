e05a23406a038db047c5fc072e01354f931fc762
_CHAI__ <apichai.freedom@gmail.com>
2020-06-13 12:18:30 +0700

    fix : remove unused routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,16 +6,4 @@ router.get('/', (req, res) => {
   res.send('Service Product.')
 })
 
-router.get('/products', (req, res) => {
-  res.send('products')
-})
-
-router.get('/products/1', (req, res) => {
-  res.send('a product')
-})
-
-router.get('*', (req, res) => {
-  res.send('Service Product, Not match any routes.')
-})
-
 export default router
